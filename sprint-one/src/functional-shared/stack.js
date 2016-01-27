0e13b3fb69bdc088ad602f1dd1ce011758f77fbf
CodeBlackwell <codeblackwell@gmail.com>
2016-01-27 12:22:44 -0800

    Functional-shared stacks in progress

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,26 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-var newStack = {};
-
-  _.extend(newStack, Stack);
-
-
-  return newStack;
+ Stack.count = 0;
+ var newStack = {};
+   _.extend(newStack, stackMethods);
+   return newStack;
+   };
+var stackMethods = {};
+stackMethods.push = function(){
+  this.count = Stack.count++;
 };
-
-Stack.stackMethods = {};
-var count = 0;
-
-Stack.stackMethods.push = function(){
-
+stackMethods.pop = function(){
+  if(Stack.count){
+    this.count = Stack.count--;
+  };
 };
-
-Stack.stackMethods.pop = function(){
-
+stackMethods.size = function(){
+  this.count = Stack.count;
+  console.log(this.count);
+return this.count;
 };
-
-Stack.stackMethods.size = function(){
-return count;
-};
-
