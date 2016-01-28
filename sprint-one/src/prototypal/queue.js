4aac165963b41081999981cb13a579164705e139
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-01-28 11:16:56 -0800

    Queue prototype complete, Begin Stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.maxIndex = 0;
+  newQueue.minIndex = 0;
+  newQueue.count    = 0;
+  return newQueue;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this[this.maxIndex] = value;[]
+  console.log("this.maxIndex within Enqueue", this.maxIndex)
+  this.count++;
+  this.maxIndex++;
+};
 
+queueMethods.dequeue = function(){
+  var dequeued = this[this.minIndex];
+  console.log("this.minIndex within dequeue", this.minIndex)
+  delete this[this.minIndex];
+  if(this.count){
+    this.count--;
+  };
+  this.minIndex++;
+  return dequeued;
+};
+
+queueMethods.size = function(){
+  return this.count;
+}
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.count = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
