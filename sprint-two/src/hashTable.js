14a0647a3f00b353ec23cbf4c2b61a14575879c6
CodeBlackwell <codeblackwell@gmail.com>
2016-01-28 12:33:01 -0800

    working on hashtab;e

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,24 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.hashStorage = {};
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(){
+  var argsLength = [].length.apply(arguments);
+  for (var i = 0; i < argsLength; i++){
+  var currentHashedVal = valueHasher(arguments[i], this._limit);
+  this.hashStorage.currentHashedVal= arguments[i];
+  console.log("index after current val assignment to storage", index)
+  };
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(index){
+  var currentHashedVal = valueHasher(index, this._limit);
 
 };
 
-HashTable.prototype.remove = function(k){
+HashTable.prototype.remove = function(index){
 
 };
 
