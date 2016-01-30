a66c1c82226f068f2de442a6a9e475038f2ad10b
CodeBlackwell <codeblackwell@gmail.com>
2016-01-29 19:44:01 -0800

    pseudocoded Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -19,37 +19,64 @@ Graph.prototype.addNode = function(node){
 
   this.nodes[this.value] = node;
 
-  console.log("vale of this.nodes[this.value]", this.nodes[this.value]);
+  console.log("value of this.nodes[this.value]", this.nodes[this.value]);
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
+  //iterate through all items in the .nodes property
+  for(var key in this.nodes){
+    if(this.nodes[key] === node){
+      return true;
+    }
+  }
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node){
+  //iterate through the nodes
+  //look for the target node to be removed
+  //delete the matching node
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
+
+  //iterate through nodes to find fromNode and toNode
+  //once found, see if both items contain pointers to each other.
+  //if so, return true
+  //else return false;
+  return false;
+
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){
+  //locate the source and 'to'Node
+  //assign each a pointer to the other
+  // from Source --> Target and Target --> Source
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
+//locate to the fromNode and toNode within the graph
+//check to see if the two nodes have an Edge. if so,
+//delete their respective pointers(edges)
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb){
+Graph.prototype.forEachNode = function(callback){
+    //iterate through all objects in nodes
+    for(var key in this.nodes){
+      //on each object use the callback function
+      callback(this.nodes[key], key, this.nodes);
+    }
 };
 
 /*
