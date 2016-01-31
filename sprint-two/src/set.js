b89ac48facebad23dbbb46c80265bf99aede6d5c
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-01-30 19:11:55 -0800

    recopy and pasted set from my old repo

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = [];
+  set.indexx = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this.storage[this.indexx] = item;
+	this.indexx++;
 };
 
 setPrototype.contains = function(item){
+	for( var i = 0 ; i < this.storage.length ; i++ ){
+		if( this.storage[i] === item ){
+			return true;
+		}
+	}
+		return false;
 };
 
 setPrototype.remove = function(item){
+	for( var i = 0 ; i < this.storage.length ; i++ ){
+		if( this.storage[i] === item ){
+			delete this.storage[i];
+		}
+	}
 };
 
 /*
